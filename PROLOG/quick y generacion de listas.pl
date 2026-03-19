223800f1a6ee0ca1da5cdde3447de6186fe6478a
pagudu <pagudu26@gmail.com>
2026-03-19 09:51:58 +0100

    Update and rename quick.pl to quick y generacion de listas.pl

--- PROLOG/quick y generacion de listas.pl
+++ PROLOG/quick y generacion de listas.pl
@@ -5,11 +5,15 @@
  Para los ejercicios, elegiremos el primer elemento.
  separamos en una lista los elementos mayores que el pivote y en otra los omenores.
  
- 
- 
  divide(+Elem, +Lista, -Menores,-Mayores):
  es ciereto cuando menores unifica con una lista que contiene los elementos de Lista que son menores o
  iguales que elem y Mayores unifica con una lista que contiene los elementos de Lista que son mayores que elem.
+ 
+ 																								19/03/2026
+###############################################################################################################
+ 
+ crea_lista(+Tam,+Num, -ListaR)
+ Genera una lista ListaR de tamaño Tam, cuyos valores sean aleatorios entre 1 y Num
  
 */
 
@@ -20,3 +24,6 @@ divide(E, [Cab|Res], Men, [Cab|R2]) :- Cab > E, divide(E,Res,Men,R2).
 quicksort([],[]).
 quicksort([Cab|R],L) :- divide(Cab,R, Men, May), quicksort(Men, Men2), quicksort(May, May2), append(Men2, [Cab|May2], L). 
 	
+
+crea_lista(0,_,[]).
+crea_lista(Tam,N,[C|R]):- Tam > 0, Tam2 is Tam - 1, crea_lista(Tam2,N,R), random_between(1,N,C).
