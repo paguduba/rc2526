7690ec8e2dd26eaa952445fc85e8f00291bbd7a5
pagudu <pagudu26@gmail.com>
2026-04-15 11:00:42 +0200

    -.-. --- .-. .-. . -.-. -.-. .. --- -. crea_abN

--- PROLOG/ARBOLES/crea_ab.pl
+++ PROLOG/ARBOLES/crea_ab.pl
@@ -5,9 +5,10 @@ es cierto cuando Arbol unifica con un arbol binario que contiene solo esas etiqu
 ARBOL BALANCEADO ESTA VEZ
 
 */
-
 arbol1(a(1,a(2,nil,nil),a(3, nil, a(4,nil,nil)))).
 
+crea_lista(0,[]).
+crea_lista(N,[N|L2]):- N > 0, N2 is N - 1, crea_lista(N2,L2).
 
 crea_ab([], nil).
 crea_ab([E], a(E,nil,nil)).
@@ -16,7 +17,4 @@ crea_ab([C|R], a(C, Hi,Hd)):- length(R, M), T is M div 2,
 				length(R1, T), append(R1,R2,R), 
 				crea_ab(R1, Hi), crea_ab(R2,Hd).
 				
-				
-crea_abN(0,nil).
-crea_abN(N, a(N,Hi,Hd)):- N>0, 0 == N mod 2, Ni is N - 1, Nd is N-2 ,crea_abN(Ni, Hi) ,  crea_abN(Nd, Hd).
-crea_abN(N, a(N,Hi,nil)):- N>0, Ni is N - 1,crea_abN(Ni, Hi) .
+crea_abN(N,A):- crea_lista(N,Lista), crea_ab(Lista,A).
