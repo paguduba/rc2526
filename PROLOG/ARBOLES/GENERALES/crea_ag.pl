8f3d7553b326c7d9d81dade52308c83dbcbdcfae
pagudu <pagudu26@gmail.com>
2026-04-22 11:41:17 +0200

    Create crea_ag.pl

---
+++ PROLOG/ARBOLES/GENERALES/crea_ag.pl
@@ -0,0 +1,23 @@
+/*
+
+crea_ag(+ListaEtiq, +N, -ArbolG)
+es cierto si ArbolG unifica con un arbol generico que contiene todas las etiquetas de ListaEtiq y tiene de maximo N hijos
+
+*/
+
+
+crea_ag([E], _ , a(E,[]) ).
+crea_ag([C|R], N , a(C,ListaA) ):- length(R,L), L=<N, 
+						crea_lista_arboles(R, ListaA).
+
+crea_ag([C|L], N , a(C,List)):- length(L2, N), append(L2,L3,L),
+						crea_ag(L2, N , ListaA), crea_ag(L3,N,ListaB), append([ListaA], [ListaB], List).
+
+/*
+crea_lista_arboles(ListaE, ListaA).
+es cierto si listaArboles unifica con una lista de arboles genericos sin hijos  y todas las etiquetas de listaEtiquetas
+*/
+
+
+crea_lista_arboles([],[]).
+crea_lista_arboles([Cab|Resto], [a(Cab,[])|R]):- crea_lista_arboles(Resto,R).
