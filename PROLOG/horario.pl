619b2303f4f508499c1ad72f4fdad5fab7e2a438
pagudu <pagudu26@gmail.com>
2026-05-14 09:49:25 +0200

    Update horario.pl

--- PROLOG/horario.pl
+++ PROLOG/horario.pl
@@ -18,17 +18,68 @@
 	Restricciones para el requerimientos: el timeslots dividido por dia, tiene que ser forzosamente ascendente, para que sea en dias diferentes <-- esto se ignora en casos de acoplamientos(?
 	
 	
-	setof: cuando tenemos soluciones multiples para una consulta, las coloca todas en una sola lista
+	setof: cuando tenemos soluciones multiples para una consulta, las coloca todas en una sola lista. NO permite duplicados
+	bagof : igual que el setof, pero permite duplicados
 */
 
-:- load_files('req1.pl').
-:- use_module(library(clpfd)).
+:- load_files('req1.pl').		%cargar requisitos
+:- use_module(library(clpfd)).	%libreria
+:- use_module(library(pairs)).	%pairs_keys()y pairs_values()
 
 requirements(Rs):-
-	Goal = class_subject_teacher_times(Class, Subject, Teacher, Number),
-	setof(req(Class,Subject,Teacher,Number), Goal, Rs0),
+	Goal = class_subject_teacher_times(Class, Subject, Teacher, Number),	%
+	setof(req(Class,Subject,Teacher,Number), Goal, Rs0), %es una "traduccion" de class_subject...
 	maplist(req_with_slots, Rs0, Rs).
 	
-	req_with_slots(R, R-Slots):- R = req(_,_,_,N), length(Slots,N).
+req_with_slots(R, R-Slots):- R = req(_,_,_,N), length(Slots,N).
+
+/*
+	2o dia: construir la estructura del sudoku
+	
+*/
+classes(Classes):-
+	setof(C, S^N^T^class_subject_teacher_times(C,S,T,N), CLasses).	%para obtener un listado de todos los grupos SIN REPETICIONES
+teachers(Teachers):-
+	setof(T, S^N^C^class_subject_teacher_times(C,S,T,N), Teachers).	
+	
+rooms(Rooms) :-
+        findall(Room, room_alloc(Room,_C,_S,_Slot), Rooms0),
+        sort(Rooms0, Rooms).
+
+requirements_variables(Rs, Vars) :-
+        requirements(Rs),
+        pairs_slots(Rs, Vars),			 				%tendremos todas las variables en una sola lista aplanada
+        slots_per_week(SPW),
+        Max #= SPW - 1,
+        Vars ins 0..Max,								%rango de 0 a 34 en este caso
+        maplist(constrain_subject, Rs),				%aplica las restricciones de asignaturas a todos los requisitos 
+        classes(Classes),				
+        teachers(Teachers),
+        rooms(Rooms),
+        maplist(constrain_teacher(Rs), Teachers),
+        maplist(constrain_class(Rs), Classes),
+        maplist(constrain_room(Rs), Rooms).
 
 
+
+slot_quotient(S, Q) :-
+        slots_per_day(SPD),
+        Q #= S // SPD.			
+
+strictly_ascending(Ls) :- chain(#<, Ls).
+
+constrain_subject(req(Class,Subj,_Teacher,_Num)-Slots) :-
+        strictly_ascending(Slots), % break symmetry
+        maplist(slot_quotient, Slots, Qs0),					%genera una nueva lista con los resultados(?
+        findall(F-S, coupling(Class,Subj,F,S), Cs),			%busca las agrupaciones
+        maplist(slots_couplings(Slots), Cs),
+        pairs_values(Cs, Seconds0),
+        sort(Seconds0, Seconds),
+        list_without_nths(Qs0, Seconds, Qs),
+        strictly_ascending(Qs).
+
+pairs_slots(Ps, Vs) :-
+        pairs_values(Ps, Vs0),
+        append(Vs0, Vs).
+
+%el chain([A,B,C], ) añade restricciones del orden de los elementos que van en la lista
